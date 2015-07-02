0a62179f245e5b81f10c9ed4e551e8f9a41ce9dd
Chris Schafer <swiss@veryoblivio.us>
2015-07-01 22:08:46 -0700

    more functions, improvements

--- insteon/insteon_functions.go
+++ insteon/insteon_functions.go
@@ -36,30 +36,50 @@ func SearchString(s string)(dev_type string, id int, loc int){//Going to search 
 	    return
 	}
     }
+    dev_type = "not_found"
+    id = 0
+    loc = -1
     return
 }
-func DeviceSearchID(dev_id int)(dev Device){
+func DeviceSearchID(dev_id int)(dev Device, success bool){
     for _, device := range DevList {
 	if device.DeviceID == dev_id {
 	    dev = device
+	    success = true
 	    return
 	}
     }
+    success = false
     return
 }
-func DeviceSearch(s string)(dev Device){
+func SceneSearch(s string)(scene_return Scene, success bool){
+    for _, scene := range SceneList {
+	s1 := strings.ToLower(scene.SceneName) 
+	if s1 == s {
+	    scene_return = scene
+	    success = true
+	    return
+	}
+    }
+    success = false
+    return
+}
+func DeviceSearch(s string)(dev Device, success bool){
     for _, device := range DevList {
 	s1 := strings.ToLower(device.DeviceName) 
 	if s1 == s {
 	    dev = device
+	    success = true
 	    return
 	}
     }
+    success = false
     return
 }
 func DeviceOn(device_id int)(success bool){
     var n Command
-    device := DeviceSearchID(device_id)
+    device, success := DeviceSearchID(device_id)
+    if success != true { return false}
     n.Command = "on"
     n.Level = device.DimLevel
     n.Device_Id = device_id
